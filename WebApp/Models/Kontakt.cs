7fca5102a8fd3a135656da9dbdd5a2954ee8303b
KaraitPL <karaitpl@gmail.com>
2024-05-19 14:56:11 +0200

    Kontakt model created

---
+++ WebApp/Models/Kontakt.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApp.Models
+{
+    public class Kontakt
+    {
+        public enum Kategoria
+        {
+            służbowy,
+            prywatny,
+            inny
+        }
+
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko {  get; set; }
+        public string Email { get; set; }
+        public string Haslo { get; set; }
+        public Kategoria Kat { get; set; }
+        public long Telefon { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        public Kontakt()
+        {
+
+        }
+    }
+}
